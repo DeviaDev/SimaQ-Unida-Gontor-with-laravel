52f6d677b9766bcacb7199f2578368ec155ab221
DeviaDev <devianestnarendra@gmail.com>
2025-10-06 13:32:44 +0700

    Update Content

--- resources/views/admin/user/create.blade.php
+++ resources/views/admin/user/create.blade.php
@@ -20,24 +20,24 @@
             
         </div>
         <div class="card-body">
-            <form action="userStore" method="post">
+            <form action="{{ route ('userStore') }}" method="post">
                 @csrf
            <div class="row mb-2">
             <div class="col-xl-6 mb-2">
                 <label class="form-label">
                     <span class="text-danger">*</span> Nama:</label>
-                <input type="text" name="name" class="form-control">
+                <input type="text" name="nama" class="form-control @error('nama') is-invalid @enderror" value="{{ old('nama') }}">
                 @error('nama')
-                <small class="text-danger">
-                    {{ $message }}
-                </small>
+                <small class="text-danger">{{ $message }}</small>
                 @enderror
+
             </div>
 
             <div class="col-xl-6">
                 <label class="form-label">
                     <span class="text-danger">*</span> Email:</label>
-                <input type="email" name="email" class="form-control">
+                <input type="email" name="email" class="form-control @error ('email') is-invalid
+                @enderror" value="{{ old('email') }}">
                 @error('email')
                 <small class="text-danger">
                     {{ $message }}
@@ -47,32 +47,58 @@
            </div>
 
            <div class="row mb-2">
-                <div class="col-xl-6" >
+                <div class="col-xl-6 mb-2" >
                 <label class="form-label">
                     <span class="text-danger">*</span> Password:</label>
-                <input type="password" name="password" class="form-control">
+                <input type="password" name="password" class="form-control @error ('password') is-invalid
+                @enderror" value="{{ old('password') }}">
                 @error('password')
                 <small class="text-danger">
                     {{ $message }}
                 </small>
                 @enderror
             </div>
-           </div>
 
-           <div class="row mb-2">
-                <div class="col-xl-6" >
+            <div class="col-xl-6" >
                 <label class="form-label">
                     <span class="text-danger">*</span> Konfimasi Password:</label>
-                <input type="password" name="password" class="form-control">
+                <input type="password" name="password_confirmation" class="form-control @error ('password') is-invalid
+                @enderror">
             </div>
            </div>
-           
-        </div>
 
-        <div class="row mb-5 justify-content-center rounded">
-            <button class="btn btn-sm btn-primary col-3" href="#">
+           <div class="row mb-5 justify-content-center rounded">
+            <button type="submit" class="btn btn-sm btn-primary col-3" href="#">
                 <i class="fas fa-save mr-2"></i>Simpan</button>
         </div>
+           
         </form>
+
+        <script>
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('input').forEach(input => {
+        input.addEventListener('input', function () {
+            // Hilangkan border merah
+            this.classList.remove('is-invalid');
+
+            // Hapus pesan error di bawah input (mencakup semua struktur)
+            const allErrors = document.querySelectorAll('.text-danger');
+            allErrors.forEach(err => {
+                // Pastikan error terkait dengan input yang sama (berada di parent yang sama)
+                if (err.closest('.col-xl-6, .mb-2, .form-group')?.contains(this)) {
+                    err.remove();
+                }
+            });
+        });
+    });
+});
+</script>
+
+
+
+
+
+
+        </div>  
     </div>
 @endsection
